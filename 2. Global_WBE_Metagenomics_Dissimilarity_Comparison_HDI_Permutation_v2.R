a3f7157d062b9a5e720c494638bc094a3c0bd1b0
Jangwoo Lee <63304222+myjackson@users.noreply.github.com>
2024-11-17 12:06:22 -0500

    Update 2. Global_WBE_Metagenomics_Dissimilarity_Comparison_HDI_Permutation_v2.R

--- 2. Global_WBE_Metagenomics_Dissimilarity_Comparison_HDI_Permutation_v2.R
+++ 2. Global_WBE_Metagenomics_Dissimilarity_Comparison_HDI_Permutation_v2.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/myjac/OneDrive - University of Calgary/1. Jangwoo_WBE/3_Data_Analysis/a_WBE_Metagenomics/15_Global_SES")
+setwd("C:/Users/.../Your Directory Path/.../15_Global_SES")
 data_folder = "GitHub"
 
 ########### CGY2021 - dissimilarity between Calgary's nerighborhoods
